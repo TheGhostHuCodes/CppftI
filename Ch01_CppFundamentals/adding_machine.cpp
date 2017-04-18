43d83c907f4e50c87388e1f0a6f752f3885ad7ce
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-04-17 18:08:38 -0700

    Have program output number count and average

--- Ch01_CppFundamentals/adding_machine.cpp
+++ Ch01_CppFundamentals/adding_machine.cpp
@@ -3,6 +3,7 @@
 int main() {
     double sum = 0.0;
     double n = 1.0;
+    int cnt = 0;
     std::cout
         << "Enter a list of numbers, terminated with a non-digit character."
         << std::endl
@@ -12,9 +13,12 @@ int main() {
         if (!std::cin) {
             break;
         }
+        cnt += 1;
         sum = sum + n;
     } while (std::cin);
 
     std::cout << "Total is: " << sum << std::endl;
+    std::cout << "Number of numbers: " << cnt << std::endl;
+    std::cout << "Average is: " << sum / cnt << std::endl;
     return 0;
 }
