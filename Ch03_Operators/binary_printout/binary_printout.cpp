9f72890d3532916d7c8dcb2c46fd5c8d14a84b9f
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-04-21 07:30:02 -0700

    Print binary form of 32-bit integers

--- Ch03_Operators/binary_printout/binary_printout.cpp
+++ Ch03_Operators/binary_printout/binary_printout.cpp
@@ -2,8 +2,8 @@
 
 int main() {
     int n = 0;
-    short test_num = 0;
-    unsigned mask = 0x8000;
+    int test_num = 0;
+    unsigned mask = 0x80000000;
     std::cout << "Enter a number: " << std::endl;
     std::cin >> test_num;
     while (n++ < sizeof(test_num) * 8) {
