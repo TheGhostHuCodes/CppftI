67d2e2357df4ca6f98127af86ef4e39ca7446662
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-04-21 07:34:42 -0700

    Use for loop to loop through binary printout

--- Ch03_Operators/binary_printout/binary_printout.cpp
+++ Ch03_Operators/binary_printout/binary_printout.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
 int main() {
-    int n = 0;
     int test_num = 0;
     unsigned mask = 0x80000000;
     std::cout << "Enter a number: " << std::endl;
     std::cin >> test_num;
-    while (n++ < sizeof(test_num) * 8) {
+    for (int n = 0; n++ < sizeof(test_num) * 8;) {
         std::cout << ((test_num & mask) != 0);
         mask >>= 1;
         if (n % 4 == 0) {
