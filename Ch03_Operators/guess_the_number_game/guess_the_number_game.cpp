2d8ae4201288054dba3f68fc99c0cedcf92dc280
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-04-21 22:43:43 -0700

    Query user at beginning of program for the max range of numbers to guess

--- Ch03_Operators/guess_the_number_game/guess_the_number_game.cpp
+++ Ch03_Operators/guess_the_number_game/guess_the_number_game.cpp
@@ -1,15 +1,29 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <limits>
 
 int main() {
     srand(time(nullptr));
     bool play_more = true;
     int target = 0;
     int guess = 0;
+    int N = 0;
+
+    do {
+        std::cout
+            << "Please enter a positive value of N to play the game with. "
+               "Numbers will be chosen between 1 and N: "
+            << std::endl;
+        std::cin >> N;
+        if (!std::cin) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    } while (N <= 0);
 
     while (play_more) {
-        target = 1 + rand() % 50;
+        target = 1 + rand() % N;
         while (true) {
             std::cout << "Guess the number, or 0 to quit: ";
             std::cin >> guess;
