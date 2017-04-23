06b656883a2b5228b682b61a5a86d389e799c664
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-04-23 11:50:31 -0700

    Use do-while loop instead of dreaded goto

--- Ch03_Operators/computer_guesses_the_number/computer_guesses_the_number.cpp
+++ Ch03_Operators/computer_guesses_the_number/computer_guesses_the_number.cpp
@@ -6,32 +6,36 @@ int main() {
     int upper_bound;
     int n = 1;
     int cmd = 1;
-top_of_program:
-    lower_bound = 1;
-    upper_bound = 50;
-    std::cout << "Think of a number from 1 to 50." << std::endl;
-    std::cout << "Then press ENTER.";
-    std::cin.ignore();
-    bool guess_again = true;
+    bool play_again = false;
+
     do {
-        n = static_cast<int>((lower_bound + upper_bound) / 2.0);
-        std::cout << "I guess " << n << "." << std::endl;
-        std::cout << "Respond: 1=Correct! 2=Guess was too low! 3=Guess was too "
-                     "high! ENTER =>";
+        lower_bound = 1;
+        upper_bound = 50;
+        std::cout << "Think of a number from 1 to 50." << std::endl;
+        std::cout << "Then press ENTER.";
+        std::cin.ignore();
+        bool guess_again = true;
+        do {
+            n = static_cast<int>((lower_bound + upper_bound) / 2.0);
+            std::cout << "I guess " << n << "." << std::endl;
+            std::cout
+                << "Respond: 1=Correct! 2=Guess was too low! 3=Guess was too "
+                   "high! ENTER =>";
+            std::cin >> cmd;
+            if (cmd == 1) {
+                std::cout << "Yay! I got it right!" << std::endl;
+                guess_again = false;
+            } else if (cmd == 3) {
+                upper_bound = n - 1;
+            } else {
+                lower_bound = n + 1;
+            }
+        } while (guess_again);
+        std::cout << "Play again? 1=Yes 2=No. ENTER:";
         std::cin >> cmd;
         if (cmd == 1) {
-            std::cout << "Yay! I got it right!" << std::endl;
-            guess_again = false;
-        } else if (cmd == 3) {
-            upper_bound = n - 1;
-        } else {
-            lower_bound = n + 1;
+            play_again = true;
         }
-    } while (guess_again);
-    std::cout << "Play again? 1=Yes 2=No. ENTER:";
-    std::cin >> cmd;
-    if (cmd == 1) {
-        goto top_of_program;
-    }
+    } while (play_again);
     return 0;
 }
