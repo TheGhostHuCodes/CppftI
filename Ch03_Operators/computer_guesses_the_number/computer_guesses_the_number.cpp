55047d983db3a79d48d29adf232f8a98f6fa6b1d
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-04-23 11:43:53 -0700

    Add a program where the computer guesses a number the user picks
    My God, I think this is the first time I have ever written a goto
    statement. Let's refactor this soon before someone notices.

---
+++ Ch03_Operators/computer_guesses_the_number/computer_guesses_the_number.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int lower_bound;
+    int upper_bound;
+    int n = 1;
+    int cmd = 1;
+top_of_program:
+    lower_bound = 1;
+    upper_bound = 50;
+    std::cout << "Think of a number from 1 to 50." << std::endl;
+    std::cout << "Then press ENTER.";
+    std::cin.ignore();
+    bool guess_again = true;
+    do {
+        n = static_cast<int>((lower_bound + upper_bound) / 2.0);
+        std::cout << "I guess " << n << "." << std::endl;
+        std::cout << "Respond: 1=Correct! 2=Guess was too low! 3=Guess was too "
+                     "high! ENTER =>";
+        std::cin >> cmd;
+        if (cmd == 1) {
+            std::cout << "Yay! I got it right!" << std::endl;
+            guess_again = false;
+        } else if (cmd == 3) {
+            upper_bound = n - 1;
+        } else {
+            lower_bound = n + 1;
+        }
+    } while (guess_again);
+    std::cout << "Play again? 1=Yes 2=No. ENTER:";
+    std::cin >> cmd;
+    if (cmd == 1) {
+        goto top_of_program;
+    }
+    return 0;
+}
