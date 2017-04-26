2d1a4147027e55da2bec009cf8f6dd808e077647
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-04-25 20:15:51 -0700

    Refactor function to have single responsibility

--- Ch06_PointersArraysAndReferences/sieve/sieve.cpp
+++ Ch06_PointersArraysAndReferences/sieve/sieve.cpp
@@ -1,7 +1,7 @@
 #include <array>
 #include <iostream>
 
-void process_prime(bool* p, int min_n, int max_n, int n);
+void process_prime(bool* p, int max_n, int n);
 
 int main() {
     int min_n = 0;
@@ -15,28 +15,25 @@ int main() {
 
     for (int i = 2; i <= max_n; ++i) {
         if (p[i]) {
-            process_prime(p, min_n, max_n, i);
+            process_prime(p, max_n, i);
         }
     }
-    std::cout << std::endl;
     int primes_found = 0;
     for (int i = min_n; i <= max_n; ++i) {
         if (p[i]) {
+            std::cout << i << "\t";
             ++primes_found;
         }
     }
+    std::cout << std::endl;
     std::cout << "Primes found: " << primes_found << std::endl;
     delete[] p;
     return 0;
 }
 
-// Print the number n passed to the function if greater than or equal to
-// min_n, then flag all the elements in the boolean array (p) that correspond
-// to a multiple of n as false.
-void process_prime(bool* p, int min_n, int max_n, int n) {
-    if (n > min_n) {
-        std::cout << n << "\t";
-    }
+// Flag all the elements in the boolean array (p) that correspond to a
+// multiple of n as false.
+void process_prime(bool* p, int max_n, int n) {
     for (int i = n + n; i <= max_n; i += n) {
         p[i] = false;
     }
