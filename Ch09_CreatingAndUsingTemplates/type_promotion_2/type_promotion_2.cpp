be0012e1d770124c2ee22ea244ec3a95eb3fcb31
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-05-04 06:53:47 -0700

    Add a function template program showing type promotion

---
+++ Ch09_CreatingAndUsingTemplates/type_promotion_2/type_promotion_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+
+template <typename T1, typename T2> std::string return_type(T1 a, T2 b) {
+    if (typeid(a + b) == typeid(int)) {
+        return "int";
+    }
+    if (typeid(a + b) == typeid(unsigned)) {
+        return "unsigned";
+    }
+    if (typeid(a + b) == typeid(float)) {
+        return "float";
+    }
+    if (typeid(a + b) == typeid(double)) {
+        return "double";
+    }
+    if (typeid(a + b) == typeid(char)) {
+        return "char";
+    }
+    return "other";
+};
+
+int main() {
+    char ch = 'a';
+    int si = 0;
+    unsigned ui = 0;
+    double x = 0.0;
+    float flt = 0.0F;
+
+    std::cout << "ch + si => " << return_type(ch, si) << std::endl;
+    std::cout << "si + si => " << return_type(si, si) << std::endl;
+    std::cout << "si + ui => " << return_type(si, ui) << std::endl;
+    std::cout << "si + x => " << return_type(si, x) << std::endl;
+    std::cout << "x + flt => " << return_type(x, flt) << std::endl;
+    return 0;
+}
