ea946525eb5a436c04d3f6cde6913d18a1d7ae2d
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-05-04 06:56:26 -0700

    Add type promotion example for unsigned and unsigned long

--- Ch09_CreatingAndUsingTemplates/type_promotion_2/type_promotion_2.cpp
+++ Ch09_CreatingAndUsingTemplates/type_promotion_2/type_promotion_2.cpp
@@ -9,6 +9,9 @@ template <typename T1, typename T2> std::string return_type(T1 a, T2 b) {
     if (typeid(a + b) == typeid(unsigned)) {
         return "unsigned";
     }
+    if (typeid(a + b) == typeid(unsigned long)) {
+        return "unsigned long";
+    }
     if (typeid(a + b) == typeid(float)) {
         return "float";
     }
@@ -25,6 +28,7 @@ int main() {
     char ch = 'a';
     int si = 0;
     unsigned ui = 0;
+    unsigned long ul = 0;
     double x = 0.0;
     float flt = 0.0F;
 
@@ -33,5 +37,6 @@ int main() {
     std::cout << "si + ui => " << return_type(si, ui) << std::endl;
     std::cout << "si + x => " << return_type(si, x) << std::endl;
     std::cout << "x + flt => " << return_type(x, flt) << std::endl;
+    std::cout << "ui + ul=> " << return_type(ui, ul) << std::endl;
     return 0;
 }
