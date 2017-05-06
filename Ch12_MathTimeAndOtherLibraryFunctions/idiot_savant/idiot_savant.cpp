cb5ecda4f08e3915775395dbd70dabb80caa0649
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-05-06 10:26:38 -0700

    Change print day calculator to accept month as string instead

--- Ch12_MathTimeAndOtherLibraryFunctions/idiot_savant/idiot_savant.cpp
+++ Ch12_MathTimeAndOtherLibraryFunctions/idiot_savant/idiot_savant.cpp
@@ -1,25 +1,43 @@
+#include <algorithm>
 #include <ctime>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 std::string input_str;
 
 int main() {
     int a_year = 0;
-    int a_month = 0;
+    std::string a_month = "";
+    int month_number = 0;
     int a_day = 1;
+
+    std::unordered_map<std::string, int> month_dict{
+        {"january", 1},   {"february", 2}, {"march", 3},     {"april", 4},
+        {"may", 5},       {"june", 6},     {"july", 7},      {"august", 8},
+        {"september", 9}, {"october", 10}, {"november", 11}, {"december", 12}};
 
     do {
         std::cout << "Enter a year from 1900 onward: ";
         std::cin >> a_year;
-        std::cout << "Enter a month from 1 to 12: ";
+        std::cout << "Enter a month from \"January\" to \"December\": ";
         std::cin >> a_month;
+        std::transform(a_month.begin(), a_month.end(), a_month.begin(),
+                       ::tolower);
+        auto got = month_dict.find(a_month);
+        if (got != month_dict.end()) {
+            month_number = got->second;
+        } else {
+            std::cout << "\"" << a_month
+                      << "\" is not a valid month. Try again." << std::endl;
+            continue;
+        }
         std::cout << "Enter a day of the month, from 1 to 31: ";
         std::cin >> a_day;
 
         tm the_day;
         the_day.tm_mday = a_day;
-        the_day.tm_mon = a_month - 1;
+        the_day.tm_mon = month_number - 1;
         the_day.tm_year = a_year - 1900;
         the_day.tm_hour = 12;
         the_day.tm_min = 0;
